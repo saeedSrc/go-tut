d103e8540948be16ac574360b8112d46b4b6485b
saeed rasooli <saeedrasooli@saeeds-MacBook-Pro.local>
2022-01-21 22:21:56 +0330

    go rountine

--- main.go
+++ main.go
@@ -1,28 +1,35 @@
 package main
 
-import "fmt"
-
-
+import (
+	"fmt"
+	"sync"
+)
 
 type person struct {
 	Firstname string
 	Lastname  string
 }
 
+var wg sync.WaitGroup
+
 func main() {
-  foo()
-  bar()
+
+	wg.Add(1)
+	go foo()
+	bar()
+
+	wg.Wait()
 }
 
-
 func foo() {
-	for  i := 0 ; i < 10 ; i ++ {
-  fmt.Println("foo:",i)
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
 	}
+	wg.Done()
 }
 
 func bar() {
-	for  i := 0 ; i < 10 ; i ++ {
-  fmt.Println("bar:",i)
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
 	}
 }
