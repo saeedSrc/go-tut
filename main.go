0eba3f135ca95a43b516d15d5c18849e63acc131
saeed rasooli <saeedrasooli@saeeds-MacBook-Pro.local>
2022-01-21 18:48:44 +0330

    call back func

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "fmt"
 
-// import "fmt"
+
 
 type person struct {
 	Firstname string
@@ -13,51 +10,20 @@ type person struct {
 }
 
 func main() {
-
-	// hello := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	// fmt.Println(sum(hello...))
-
-	// fmt.Println(odd(sum, hello...))
-
-	saeed := person{
-		"saeed",
-		"rasooli",
-	}
-
-	fmt.Println(saeed)
-
-	saeedmarshall, err := json.Marshal(saeed)
-	if err != nil {
-		fmt.Printf("%V", err)
-	}
-	fmt.Println(string(saeedmarshall))
-
-	newsaeed := person{
-		"saeed",
-		"rasooli",
-	}
-	json.Unmarshal(saeedmarshall, &newsaeed)
-
-	fmt.Println(newsaeed)
+  foo()
+  bar()
 
 }
 
-func sum(h ...int) int {
-	total := 0
-	for _, v := range h {
-		total += v
+
+func foo() {
+	for  i := 0 ; i < 10 ; i ++ {
+  fmt.Println("foo:",i)
 	}
-	return total
 }
 
-func odd(f func(...int) int, h ...int) int {
-	oddarray := []int{}
-	for _, v := range h {
-		if v%2 != 0 {
-			oddarray = append(oddarray, v)
-		}
+func bar() {
+	for  i := 0 ; i < 10 ; i ++ {
+  fmt.Println("bar:",i)
 	}
-
-	return f(oddarray...)
 }
