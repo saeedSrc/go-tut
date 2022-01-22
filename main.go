094715898da89fdaa744be3e7184db7542d5f552
saeed rasooli <saeedrasooli@saeeds-MacBook-Pro.local>
2022-01-22 18:00:27 +0330

    cاشددثمس

--- main.go
+++ main.go
@@ -3,21 +3,48 @@ package main
 import "fmt"
 
 func main() {
-	var chan1 = make(chan int, 10)
 
-	// go func() {
-		for i := 0; i < 10; i++ {
-			chan1 <- i
+	var chan1 = make(chan int)
+	var chan2 = make(chan int)
+	var chan3 = make(chan int)
 
-			fmt.Println(<-chan1)
+	go send(chan1, chan2, chan3)
+	receive(chan1, chan2, chan3)
+
+	fmt.Println("about to exit")
+}
+
+func receive(e, o, q <-chan int) {
+
+	for {
+		select {
+		case v := <-e:
+			fmt.Println(v)
+
+		case v := <-o:
+			fmt.Println(v)
+
+		case v := <-q:
+			fmt.Println(v)
+			return
 		}
-		// }()
-		
-		
-		go func ()  {
-			for v := range chan1 {
-				fmt.Println(v)
-			}
-			}()
-			close(chan1)
+	}
+
+}
+
+func send(e, o, q chan<- int) {
+
+	for i := 0; i < 11; i++ {
+
+		if i%2 == 0 {
+			e <- i
+		} else {
+			o <- i
+		}
+	}
+	close(e)
+	close(o)
+	q <- 0
+	close(q)
+
 }
