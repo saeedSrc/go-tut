9d6af3a1c8aa6984a8c3d56d3f7caeed164ccf95
saeed rasooli <saeedrasooli@saeeds-MacBook-Pro.local>
2022-01-23 23:38:10 +0330

    go routin02

--- main.go
+++ main.go
@@ -1,57 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"math/rand"
-	"sync"
+	"runtime"
 	"time"
 )
 
 func main() {
 
-	var ch1 = make(chan int)
-	var ch2 = make(chan int)
-	go populate(ch1)
+	var ctx = context.Background()
 
-	go fanoutin(ch1, ch2)
+	var ctx2, cancel = context.WithCancel(ctx)
 
-	for v:=range ch2 {
-		fmt.Println(v)
-	}
+
+	go func() {
+		n := 0
+		for {
+			select {
+			case <-ctx2.Done():
+				return
+			default:
+				n++
+				time.Sleep(time.Microsecond * 100)
+                fmt.Println("working:", n)
+			}
+		}
+	} ()
+
+
+
+	time.Sleep(2 * time.Second)
+
+	cancel()
+
+
+
+	fmt.Println(runtime.NumGoroutine())
 
     fmt.Println("exit")
-}
-
-
-func populate(c chan int) { 
-	for i:=0;i<10;i++ {
-		     c<-i
-	}
-	close(c)
-}
-
-
-func fanoutin(c1, c2 chan int) {
-
-
-	var wg  sync.WaitGroup
-
-	for v:= range c1 {
-		wg.Add(1)
-		go func (v2 int)  {
-			c2<-timeConsumingWork(v2)
-			wg.Done()
-		}(v)
-   
-	}
- 
-	wg.Wait()
-	close(c2)
-}
-
-func timeConsumingWork(v int) int {
-
-	time.Sleep(time.Microsecond * 10)
-
-	return v + rand.Intn(500)
 }
