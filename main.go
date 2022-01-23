a93b528689055e3c7c44925a4a880a900028f1cc
saeed rasooli <saeedrasooli@saeeds-MacBook-Pro.local>
2022-01-23 23:07:26 +0330

    go routing1

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 
@@ -8,43 +11,61 @@ func main() {
 	var chan2 = make(chan int)
 	var chan3 = make(chan int)
 
-	go send(chan1, chan2, chan3)
-	receive(chan1, chan2, chan3)
 
-	fmt.Println("about to exit")
-}
+	go send(chan1, chan2)
 
-func receive(e, o, q <-chan int) {
 
-	for {
-		select {
-		case v := <-e:
-			fmt.Println(v)
+	go receive(chan1, chan2, chan3)
 
-		case v := <-o:
-			fmt.Println(v)
-
-		case v := <-q:
-			fmt.Println(v)
-			return
-		}
+	for v:= range chan3 {
+		fmt.Println(v)
 	}
 
+
+	fmt.Println("abou to ecxit")
+
 }
 
-func send(e, o, q chan<- int) {
-
-	for i := 0; i < 11; i++ {
-
+func send(e, o chan<- int){
+	for i:=0; i<11; i++ {
 		if i%2 == 0 {
-			e <- i
+          e<- i
 		} else {
-			o <- i
+			o<-i
 		}
+
+		
 	}
 	close(e)
-	close(o)
-	q <- 0
-	close(q)
+		close(o)
+}
+
+func receive(e, o <-chan int, fan chan<- int){
+	var wg  sync.WaitGroup
+
+	wg.Add(2)
+
+	go func(){
+		for v:= range e {
+			fan <- v
+		}
+
+		wg.Done()
+
+	}()
+
+	go func(){
+		for v:= range o {
+			fan <- v
+		}
+
+		wg.Done()
+		}()
+
+		wg.Wait()
+
+		close(fan)
 
 }
+
+	
