fa29aa75e163debc67aa15a310abbfd7a8f100e6
saeed rasooli <saeedrasooli@saeeds-MacBook-Pro.local>
2022-01-26 18:01:35 +0330

    packaging

--- main.go
+++ main.go
@@ -1,37 +1,9 @@
-
 package main
 
-// import some
-import (
-	"fmt"
-)
-
-var costumizeErr = fmt.Errorf("this is formatted err : %v", "format")
-
-type devide struct {
-	first int
-	last  int
-	text  string
-}
-
+import "fmt"
 
 func main() {
-  saeed := devide {
-	  1,2, "salam",
-  }
 
-  saeed.Devider(1, 2)
-}
+	fmt.Println("the main methid")
 
-//Devider This is devider package
-func (d devide) Devider(f, l int) (result float64,  err error) {
-	if l == 0 {
-		return 0, costumizeErr
-	}
-
-	return 1, nil
-}
-
-func (d devide) Error() string {
-	return d.text
 }
