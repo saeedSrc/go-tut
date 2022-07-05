3439a09c01214d1446f7e6c5a2df51236f68f291
saeed rasooli <saeedrasooli@saeeds-MacBook-Pro.local>
2022-07-05 14:05:19 +0430

    change notification text

---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestADD (t *testing.T){
+
+    got := Add(4, 6)
+    want := 10
+
+    if got != want {
+        t.Errorf("got %d, wanted %d", got, want)
+    }
+	
+}
+
+func BenchmarkAdd(b *testing.B){
+	fmt.Println(b.N)
+    for i :=0; i < b.N ; i++{
+        Add(4, 6)
+    }
+}
